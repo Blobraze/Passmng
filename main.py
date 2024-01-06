b7851d39beec04daec5bd16e252be9b2fc84f1e0
Blobraze <virusmati123@gmail.com>
2024-01-06 12:05:13 +0100

    added logo, removed pyfiglet

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import rich
-import pyfiglet
 from rich.console import Console
 
-
-title = pyfiglet.figlet_format('Ciphera', 'big')
+title = '''
+  _____ _       _                    
+ / ____(_)     | |                   
+| |     _ _ __ | |__   ___ _ __ __ _ 
+| |    | | '_ \| '_ \ / _ \ '__/ _` |
+| |____| | |_) | | | |  __/ | | (_| |
+ \_____|_| .__/|_| |_|\___|_|  \__,_|
+         | |                         
+         |_|                         
+'''
 
 def main_menu():
     console = Console()
     console.print(f'[red]{title}[/red]', style='bold')
     
-    console.print('[red]Choose what to do:[/red]')
-    console.print('[red]1.   Generate Password[/red]')
-    console.print('[red]2.   Add Password[/red]')
-    console.print('[red]3.   Password List[/red]')
-    option = console.input('[red][1,2,3]> [/red]')
+    console.print(f'[red]Choose what to do:[/red]')
+    console.print(f'[red]1.   Generate Password[/red]')
+    console.print(f'[red]2.   Add Password[/red]')
+    console.print(f'[red]3.   Password List[/red]')
+    option = console.input(f'[red][1,2,3]> [/red]')
 
 
 if __name__=='__main__':
